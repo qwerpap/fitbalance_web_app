1e737db3dc40b7cc92032afbbb7519c5868ea1b5
qwerpap <danilshchavelev@bk.ru>
2025-03-01 18:43:57 +0600

    register/RegisterController

---
+++ src/main/kotlin/features/login/LoginController.kt
@@ -0,0 +1,10 @@
+package com.example.features.login
+
+import io.ktor.server.application.ApplicationCall
+
+class LoginController (private val call: ApplicationCall) {
+
+    fun perform() {
+
+    }
+}
